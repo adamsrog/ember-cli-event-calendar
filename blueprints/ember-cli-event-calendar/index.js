b9716f351db3ba898795a6859adfb786ce392b4c
Roger Adams <adamsrog04@gmail.com>
2015-03-03 12:23:20 -0800

    include bower dep in blueprint afterInstall()

--- blueprints/ember-cli-event-calendar/index.js
+++ blueprints/ember-cli-event-calendar/index.js
@@ -1,5 +1,9 @@
 module.exports = {
 	normalizeEntityName: function(entityName)  {
 		return entityName;
+	},
+
+	afterInstall: function() {
+		return this.addBowerPackageToProject("jquery-event-calendar", "adamsrog/jQueryEventCalendar")
 	}
 };
