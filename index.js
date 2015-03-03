bee04bb4a8a8c472948c1aa8cf957b1c9dafcb32
Roger Adams <adamsrog04@gmail.com>
2015-03-02 16:07:00 -0800

    add in stylesheet

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-event-calendar'
+  name: 'ember-cli-event-calendar',
+	
+	included: function(app) {
+		this._super.included(app);
+
+		app.import(path.join(app.bowerDirectory, '/moment/moment.js'));
+		app.import(app.bowerDirectory + '/ember-cli-moment-shim/moment-shim.js', {
+			exports: {
+				moment: ['default']
+			}
+		});
+
+		app.import(path.join(app.bowerDirectory, '/jquery-event-calendar/css/eventCalendar.css'));
+		app.import(path.join(app.bowerDirectory, '/jquery-event-calendar/css/eventCalendar_theme_flat.css'));
+		app.import(path.join(app.bowerDirectory, '/jquery-event-calendar/js/jquery.eventCalendar.js'));
+	}
 };
